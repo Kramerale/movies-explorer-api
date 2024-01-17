02863f882522960714d1144478c046a9f64cb1d7
Kramerale <aliceshoolz96@gmail.com>
2024-01-17 14:37:54 +0400

    change cors configuration in app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'https://kramerale.diploma.nomoredomainsmonster.ru',
+  origin: ['http://localhost:3001', 'https://kramerale.diploma.nomoredomainsmonster.ru'],
 }));
 
 app.use(requestLogger);
